045dc5493ca73a81849f106b3e5a3994fcb59393
Emily Wirtz <ewirtz3@gmail.com>
2020-06-20 11:35:51 -0500

    sign up post method working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,8 +4,6 @@ const db = require("../models");
 const htmlRoutes = new Router();
 
 htmlRoutes.get("/", async (_, res) => {
- 
-
   res.render("index");
 });
 
@@ -20,7 +18,7 @@ htmlRoutes.get("/users/:id", async (req, res) => {
   const this_user = await db.Users.findOne(options);
 
   res.render("profile", {
-    user: this_user,
+    username: this_user,
   });
 });
 

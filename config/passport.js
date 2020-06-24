be42d68e83a6cb9871f5ad99eb101742ca413df8
Emily Wirtz <ewirtz3@gmail.com>
2020-06-23 19:41:14 -0500

    removing validPassword

--- config/passport.js
+++ config/passport.js
@@ -1,47 +1,46 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-const db = require('../models')
+const db = require("../models");
 
-passport.use(new LocalStrategy(
+passport.use(
+  new LocalStrategy(
     // Users will sign in using an email.
     {
-      usernameField: "email"
+      usernameField: "email",
     },
     (email, password, done) => {
-    
       db.User.findOne({
         where: {
-          email: email
-        }
+          email: email,
+        },
       }).then((dbUser) => {
-        // If findOne of user email does not return valid entry then it will return incorrect email. 
+        // If findOne of user email does not return valid entry then it will return incorrect email.
         if (!dbUser) {
           return done(null, false, {
-            message: "Incorrect email."
+            message: "Incorrect email.",
           });
         }
-        // Returns incorrect password is email is correct, but password does not match. 
-        else if (!dbUser.validPassword(password)) {
-          return done(null, false, {
-            message: "Incorrect password."
-          });
-        }
+        // Returns incorrect password is email is correct, but password does not match.
+        // else if (!dbUser.validPassword(password)) {
+        //   return done(null, false, {
+        //     message: "Incorrect password."
+        //   });
+        // }
         // If both password and email are correct, return user
         return done(null, dbUser);
       });
     }
-  ));
-  
-  // Serialize is used to keep authentication accros all html endpoints.
-  passport.serializeUser(function(user, cb) {
-    cb(null, user);
-  });
-  
-  passport.deserializeUser(function(obj, cb) {
-    cb(null, obj);
-  });
-  
-  
-  module.exports = passport;
-  
+  )
+);
+
+// Serialize is used to keep authentication accros all html endpoints.
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+module.exports = passport;
